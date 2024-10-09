59c8d6a2b47d63d80ecfc80d43e22d883788c0c2
pvannierop <pim@thehyve.nl>
2024-10-09 12:30:07 +0200

    Up radarCommons to 1.1.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -24,7 +24,7 @@ object Versions {
 
     const val kotlin = "1.9.10"
 
-    const val radarCommons = "1.1.1"
+    const val radarCommons = "1.1.2"
     const val radarJersey = "0.10.0"
     const val postgresql = "42.6.0"
     const val ktor = "2.3.5"
